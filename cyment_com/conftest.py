5c01feefddad93d2ed214fdbae5db4a266feff96
Alan Cyment <acyment@MacbookAlan.fibertel.com.ar>
2023-06-09 09:57:45 -0300

    Creado con cookiecutter

---
+++ cyment_com/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cyment_com.users.models import User
+from cyment_com.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
