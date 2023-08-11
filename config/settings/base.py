b524601d6852a401e276c9bdff4aa4a5a967b0eb
Alan Cyment <acyment@gmail.com>
2023-08-11 08:48:07 -0300

    Removed bootstrap

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,7 +71,6 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
-    "crispy_bootstrap5",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
@@ -190,8 +189,7 @@ TEMPLATES = [
 FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 
 # http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = "bootstrap5"
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
 
 # FIXTURES
 # ------------------------------------------------------------------------------
