ceecdbe1b8c7f264442eb163a03f1cad3d54d752
Alan Cyment <acyment@gmail.com>
2023-11-08 22:47:27 -0300

    pre-commit now runs locally

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 from pathlib import Path
 
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     from django.conf import settings
