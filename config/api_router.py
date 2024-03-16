1ccba7dde31ba58beec106119f25eda5b4e963e2
Alan Cyment <acyment@gmail.com>
2024-03-16 12:02:46 -0300

    Pago Stripe y webhooks MP-Stripe

--- config/api_router.py
+++ config/api_router.py
@@ -4,7 +4,12 @@ from rest_framework.routers import SimpleRouter
 from django.urls import path
 
 from cyment_com.users.api.views import UserViewSet
-from cyment_com.pagos.api.views import CreateMpPreferenceView
+from cyment_com.pagos.api.views import (
+    CreateMpPreferenceView,
+    CreateStripeCheckoutSessionView,
+    MPPaymentConfirmationView,
+    StripePaymentConfirmationView,
+)
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
@@ -18,5 +23,20 @@ urlpatterns += [
         "create-mp-preference/",
         CreateMpPreferenceView.as_view(),
         name="create-mp-preference",
+    ),
+    path(
+        "create-stripe-checkoutsession/",
+        CreateStripeCheckoutSessionView.as_view(),
+        name="create-stripe-checkoutsession",
+    ),
+    path(
+        "mp-payment-confirmation/",
+        MPPaymentConfirmationView.as_view(),
+        name="mp-payment-confirmation",
+    ),
+    path(
+        "stripe-payment-confirmation/",
+        StripePaymentConfirmationView.as_view(),
+        name="stripe-payment-confirmation",
     ),
 ]
--- cyment_com/pagos/api/views.py
+++ cyment_com/pagos/api/views.py
@@ -1,11 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django.shortcuts import redirect
 import mercadopago
+import stripe
+import hashlib
+import hmac
+from django.http import HttpResponse
+import time
+import environ
 
-sdk = mercadopago.SDK(
-    "TEST-5750996062324782-052509-fcd854e6360644a3a1fee65744c25e56-77699069"
-)
+env = environ.Env()
+
+
+YOUR_DOMAIN = env("LOCAL_HOST_TUNNEL")
 
 
 class CreateMpPreferenceView(APIView):
@@ -25,13 +33,14 @@ class CreateMpPreferenceView(APIView):
                 }
             ],
             "back_urls": {
-                "success": "https://181.12.202.157/finished-payment",
-                "pending": "https://181.12.202.157/finished-payment",
-                "failure": "https://181.12.202.157/finished-payment",
+                "success": YOUR_DOMAIN + "/finished-payment",
+                "pending": YOUR_DOMAIN + "/finished-payment",
+                "failure": YOUR_DOMAIN + "/finished-payment",
             },
-            "notification_url": "https://181.12.202.157/api/webhook-mp/",
+            "notification_url": YOUR_DOMAIN + "/api/webhook-mp/",
             # Additional settings can be added here (e.g., payment methods, external references)
         }
+        sdk = mercadopago.SDK(env("MP_ACCESS_TOKEN"))
         preference_response = sdk.preference().create(preference_data)
         # Check if the preference was created successfully
         if preference_response["status"] == 201:
@@ -54,3 +63,125 @@ class CreateMpPreferenceView(APIView):
                 },
                 status=400,
             )
+
+
+class CreateStripeCheckoutSessionView(APIView):
+    # Disable authentication
+    authentication_classes = []
+    # Disable permission checks
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        try:
+            stripe.api_key = env("STRIPE_API_KEY")
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                        "price": "price_1NBdhjAXZPf5d77ewsFM3qQ5",
+                        "quantity": 1,
+                    },
+                ],
+                mode="payment",
+                success_url=YOUR_DOMAIN + "?status=approved",
+                cancel_url=YOUR_DOMAIN + "?status=canceled",
+            )
+        except Exception as e:
+            return str(e)
+
+        return redirect(checkout_session.url, code=303)
+
+
+class MPPaymentConfirmationView(APIView):
+    # Disable authentication
+    authentication_classes = []
+    # Disable permission checks
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        # La clave secreta de Mercado Pago
+        mpWebhookSecret = env("MP_WEBHOOK_SECRET")
+
+        # Extraer el header 'x-signature'
+        x_signature = request.headers.get("x-signature")
+        if not x_signature:
+            return HttpResponse("x-signature header missing", status=400)
+
+        # Dividir el contenido del header 'x-signature' para extraer 'ts' y 'v1'
+        signature_parts = {
+            part.split("=")[0]: part.split("=")[1] for part in x_signature.split(",")
+        }
+        ts = signature_parts.get("ts")
+        v1 = signature_parts.get("v1")
+
+        if not ts or not v1:
+            return HttpResponse("Invalid x-signature format", status=400)
+
+        # Construir el string final basado en el template
+        signedTemplate = (
+            "id:"
+            + request.data["data"]["id"]
+            + ";request-id:"
+            + request.headers.get("x-request-id")
+            + ";ts:"
+            + ts
+            + ";"
+        )
+
+        # Calcular la contraclave
+        cyphedSignature = hmac.new(
+            mpWebhookSecret.encode(), signedTemplate.encode(), hashlib.sha256
+        ).hexdigest()
+
+        # Comparar la clave generada con la clave extraída del header
+        if cyphedSignature != v1:
+            return HttpResponse("Invalid signature", status=403)
+
+        # Verificar el timestamp para evitar ataques de repetición
+        current_ts = int(time.time() * 1000)
+        received_ts = int(ts)
+        # Asumiendo una tolerancia de 5 minutos (300 segundos)
+        if abs(current_ts - received_ts) > 300000:
+            return HttpResponse("Timestamp validation failed", status=403)
+        print("MP!: ", cyphedSignature, v1, current_ts, received_ts)
+
+        return HttpResponse("Notificación recibida y validada", status=200)
+
+
+class StripePaymentConfirmationView(APIView):
+    # Disable authentication
+    authentication_classes = []
+    # Disable permission checks
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        payload = request.body
+        sig_header = request.headers["stripe-signature"]
+        event = None
+
+        try:
+            stripe.api_key = env("STRIPE_API_KEY")
+            stripeWebhookSecret = env("STRIPE_WEBHOOK_SECRET")
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, stripeWebhookSecret
+            )
+        except ValueError as e:
+            # Invalid payload
+            print("Error parsing payload: {}".format(str(e)))
+            return HttpResponse(status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            print("Error verifying webhook signature: {}".format(str(e)))
+            return HttpResponse(status=400)
+
+        # Handle the event
+        if event.type == "payment_intent.succeeded":
+            # payment_intent = event.data.object  # contains a stripe.PaymentIntent
+            print("PaymentIntent was successful!")
+            # Then define and call a method to handle the successful payment intent.
+            # handle_payment_intent_succeeded(payment_intent)
+        # ... handle other event types
+        else:
+            print("Unhandled event type {}".format(event.type))
+
+        return HttpResponse(status=200)
