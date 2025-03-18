8e8790264badc389964df1a50432d52b66c31483
Alan Cyment <acyment@gmail.com>
2025-03-18 13:48:04 -0300

    First version of spinner

--- frontend/src/components/Cursos.jsx
+++ frontend/src/components/Cursos.jsx
@@ -15,6 +15,7 @@ const Cursos = () => {
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [triggerScroll, setTriggerScroll] = useState(false);
   const posthog = usePostHog();
+  const [loading, setLoading] = useState(true);
   const [sliderRef, instanceRef] = useKeenSlider(
     {
       slideChanged() {},
@@ -28,6 +29,7 @@ const Cursos = () => {
   );
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         // Replace 'your_api_endpoint' with the actual endpoint URL
         const response = await axios.get('/api/tipos-de-curso');
@@ -38,6 +40,8 @@ const Cursos = () => {
         setTiposCurso(dictTiposCurso);
       } catch (error) {
         console.error('Error fetching data: ', error);
+      } finally {
+        +setLoading(false);
       }
     };
 
@@ -72,43 +76,57 @@ const Cursos = () => {
       ref={refListaCursos}
     >
       <div className="CursosHeader">
-        <ToggleGroup.Root
-          ref={sliderRef}
-          type="single"
-          onValueChange={updateSelectedItem}
-          className="ResumenCursosCarousel"
-        >
-          {Object.values(tiposCurso).map((tipoCurso) => (
-            <ToggleGroup.Item
-              className="ToggleResumenCurso"
-              value={tipoCurso.nombre_corto}
-            >
-              <div className="ContenedorImagenResumenCurso">
-                <img
-                  src={
-                    selectedCourse === tipoCurso.nombre_corto
-                      ? `static/images/${tipoCurso.foto}`
-                      : `static/images/${tipoCurso.foto_tint}`
-                  }
-                  className="ImagenResumenCurso"
-                />
-              </div>
+        {loading ? (
+          <div
+            className="LoaderContainer"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '40px',
+            }}
+          >
+            <CircleLoader color="#36D7B7" size={60} />
+          </div>
+        ) : (
+          <ToggleGroup.Root
+            ref={sliderRef}
+            type="single"
+            onValueChange={updateSelectedItem}
+            className="ResumenCursosCarousel"
+          >
+            {Object.values(tiposCurso).map((tipoCurso) => (
+              <ToggleGroup.Item
+                className="ToggleResumenCurso"
+                value={tipoCurso.nombre_corto}
+              >
+                <div className="ContenedorImagenResumenCurso">
+                  <img
+                    src={
+                      selectedCourse === tipoCurso.nombre_corto
+                        ? `static/images/${tipoCurso.foto}`
+                        : `static/images/${tipoCurso.foto_tint}`
+                    }
+                    className="ImagenResumenCurso"
+                    alt={tipoCurso.nombre_corto}
+                  />
+                </div>
 
-              <div className="TextoResumenCurso">
-                <h3 className="CursosTituloAcronimo">
-                  {tipoCurso.nombre_corto}
-                </h3>
-                <h4 className="CursosTitulo">{tipoCurso.nombre_completo}</h4>
-                <p className="CursosTituloBajada">
-                  {tipoCurso.resumen_una_linea}
-                </p>
-              </div>
-              <span className="CircleButton CircleButtonCursos" />
-            </ToggleGroup.Item>
-          ))}
-        </ToggleGroup.Root>
+                <div className="TextoResumenCurso">
+                  <h3 className="CursosTituloAcronimo">
+                    {tipoCurso.nombre_corto}
+                  </h3>
+                  <h4 className="CursosTitulo">{tipoCurso.nombre_completo}</h4>
+                  <p className="CursosTituloBajada">
+                    {tipoCurso.resumen_una_linea}
+                  </p>
+                </div>
+                <span className="CircleButton CircleButtonCursos" />
+              </ToggleGroup.Item>
+            ))}
+          </ToggleGroup.Root>
+        )}
       </div>
-      {selectedCourse && (
+      {selectedCourse && !loading && (
         <DetalleCurso
           ref={refDetalleCurso}
           tipoCurso={tiposCurso[selectedCourse]}
