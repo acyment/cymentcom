5894666c5e210c3ca61cc645b35aada61e52f95c
Alan Cyment <acyment@gmail.com>
2025-04-05 19:25:03 -0300

    refactor: simplify email sending logic

--- cursos/emails.py
+++ cursos/emails.py
@@ -1,32 +1,9 @@
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from mjml import mjml2html
 
 
 class EmailSender:
     @staticmethod
-    def send_welcome_email(inscripciones):
-        from_email = "alan@cyment.com"
-        text_content = "Probando texto solo"
-        for inscripcion in inscripciones:
-            html_body = render_to_string(
-                "emails/welcome_email.html",
-                {"inscripcion": inscripcion},
-            )
-
-            subject = (
-                f"Bienvenida al {inscripcion.curso.tipo.nombre_completo} "
-                + inscripcion.curso.fecha.strftime("%B %Y")
-            )
-            recipient_list = [
-                inscripcion.alumno.email,
-            ]
-
-            email = EmailMultiAlternatives(
-                subject,
-                text_content,
-                from_email,
-                recipient_list,
-            )
-            email.attach_alternative(html_body, "text/html")
-
-            email.send()
+    def send_welcome_email(inscripcion):
+        pass
