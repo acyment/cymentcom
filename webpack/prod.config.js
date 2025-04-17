ac053eb146ebdc07b5b71f832e93d2bdd5270413
Alan Cyment <acyment@gmail.com>
2025-04-17 20:12:17 -0300

    Added webpack speedmeasure plugin

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,21 +3,25 @@ const commonConfig = require('./common.config');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const webpack = require('webpack');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 
 // This variable should mirror the one from config/settings/production.py
 const staticUrl = '/static/';
+const smp = new SpeedMeasurePlugin();
 
-module.exports = merge(commonConfig, {
-  mode: 'production',
-  devtool: 'source-map',
-  bail: true,
-  output: {
-    publicPath: `${staticUrl}webpack_bundles/`,
-  },
-  plugins: [
-    new Dotenv({
-      path: path.resolve(__dirname, '../.envs/.production/.webpack'),
-    }),
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
-  ],
-});
+module.exports = smp.wrap(
+  merge(commonConfig, {
+    mode: 'production',
+    devtool: 'source-map',
+    bail: true,
+    output: {
+      publicPath: `${staticUrl}webpack_bundles/`,
+    },
+    plugins: [
+      new Dotenv({
+        path: path.resolve(__dirname, '../.envs/.production/.webpack'),
+      }),
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+    ],
+  }),
+);
