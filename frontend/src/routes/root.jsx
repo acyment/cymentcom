6d937174f06633b6f105e59642cc70b674181688
Alan Cyment <acyment@gmail.com>
2025-04-18 16:41:53 -0300

    RouterDebugger prod

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import App from '../components/App';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <App>
       <Outlet /> {/* Renders child routes */}
+      <TanStackRouterDevtools />
     </App>
-  )
+  ),
 });
