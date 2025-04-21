c09ce303b03b1398dd4d030681c2c153de65f3b9
Alan Cyment <acyment@gmail.com>
2025-04-21 18:48:52 -0300

    Removed references to AWS S3

--- config/settings/production.py
+++ config/settings/production.py
@@ -252,14 +252,3 @@ CSRF_TRUSTED_ORIGINS = [
     # Add the origin reported in the error message
     "https://mercadopago.com.ar",
 ]
-
-# File Storage
-# ------------------------------------------------------------------------------
-INSTALLED_APPS += ["storages"]  # noqa: F405
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = "public-read"
