28a01a009b0294f6c273b981bb8eae09a8f22527
Alan Cyment <acyment@gmail.com>
2025-04-24 14:51:59 -0300

    Added "beta" sign

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { RoughNotation } from 'react-rough-notation';
 
 const Logo = () => {
-  return <img src="static/images/logo.svg" className="Logo"></img>;
+  return (
+    <>
+      <img src="static/images/logo.svg" className="Logo"></img>
+      <RoughNotation
+        type="circle"
+        show={true}
+        color="#7b68ee"
+        strokeWidth={2.5}
+        padding={12}
+      >
+        <span className="cartelBeta" title="Versión beta">
+          Versión beta
+        </span>
+      </RoughNotation>
+    </>
+  );
 };
 
 export default Logo;
